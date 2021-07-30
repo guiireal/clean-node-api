3b0432749ab5b9c9246c084c52c1af75dd02be8e
Guilherme França <guii_real@hotmail.com>
2021-07-30 03:57:01 -0300

    feat: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -212,4 +212,24 @@ describe("SignUpController", () => {
       password: "any_password",
     });
   });
+
+  test("Should return 200 if valid data is provided", () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        name: "valid_any_name",
+        email: "valid_any_email@mail.com",
+        password: "valid_any_password",
+        passwordConfirmation: "valid_any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual({
+      id: "valid_id",
+      name: "valid_name",
+      email: "valid_email@mail.com",
+      password: "valid_password",
+    });
+  });
 });
