16e6a2b83df2b78db101962bc8e71b0d8c1b233a
guiireal <guii_real@hotmail.com>
2023-03-13 12:34:03 -0300

    test: mock returns

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,10 +1,11 @@
-import { InvalidParamError } from "../errors/invalid-param-error";
-import { MissingParamError } from "../errors/missing-param-error";
-import { ServerError } from "../errors/server-error";
-import { badRequest } from "../helpers/http-helper";
-import { Controller } from "../protocols/controller";
-import { EmailValidator } from "../protocols/email-validator";
-import { HttpRequest, HttpResponse } from "../protocols/http";
+import { InvalidParamError, MissingParamError } from "../errors";
+import { badRequest, serverError } from "../helpers/http-helper";
+import {
+  Controller,
+  EmailValidator,
+  HttpRequest,
+  HttpResponse,
+} from "../protocols";
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
@@ -32,10 +33,7 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError("email"));
       }
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError(),
-      };
+      return serverError();
     }
   }
 }
