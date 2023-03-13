6f5ade7dc178738d97ba55d1e4dd965a05483b30
guiireal <guii_real@hotmail.com>
2023-03-12 21:56:36 -0300

    chore: add signup test

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from "./signup";
+
+describe("SignUpController", () => {
+  test("Should return 400 if no name is provided", () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        email: "any_email@email.com",
+        password: "any_password",
+        password_confirmation: "any_password",
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
